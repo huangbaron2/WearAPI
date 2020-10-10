d35a63fc00b3d04b764c74e8a7734be8e637b247
huangbaron2 <huangbaron2@gmail.com>
2020-10-10 18:59:45 -0400

    Updates

---
+++ precache-manifest.90e4eba9dc75c555abf41b43978e8c72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9edbaf917408f571f93320f3a4dd17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae4832ab8956fe5b3077",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6a76f1f2b8ba56c23b64",
+    "url": "/static/css/main.ae2d6e5b.chunk.css"
+  },
+  {
+    "revision": "ae4832ab8956fe5b3077",
+    "url": "/static/js/2.eeaf59a5.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.eeaf59a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a76f1f2b8ba56c23b64",
+    "url": "/static/js/main.c7cf537a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ddbfcd34f28ce752a774ba2ae575f9d9",
+    "url": "/static/media/hookshirts.ddbfcd34.jpg"
+  }
+]);
