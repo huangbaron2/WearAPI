9c382e4738bca1b12bd5e7416ff6ceeec2565b7a
huangbaron2 <huangbaron2@gmail.com>
2020-10-10 19:22:34 -0400

    added hashrouter

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import FirstPage from './FirstPage'
 import Post from './Post'
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, HashRouter,  } from 'react-router-dom'
 
 function App() {
   //basename={process.env.PUBLIC_URL}
   return (
     <div >
-        <Router>
+        <HashRouter>
           <Route exact path = "/" component = { FirstPage }/>
-          <Route exact path = "/Post" component = { Post }/>
-        </Router>
+          <Route path = "/Post" component = { Post }/>
+        </HashRouter>
     </div>
   );
 }
