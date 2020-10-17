2fffe64956248b13ab02a18b8bf465d8e74831bb
huangbaron2 <huangbaron2@gmail.com>
2020-10-16 22:00:53 -0400

    removed error in index

--- express/index.js
+++ express/index.js
@@ -32,7 +32,6 @@ app.get('/', async (req, res) => {
 
 const path = require('path');
 const { url } = require('inspector');
-const { DataUsageOutlined, DataUsage } = require('@material-ui/icons');
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
