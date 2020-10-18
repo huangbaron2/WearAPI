fe02e2cb095012b3eeafa8578466a3284d9850c6
huangbaron2 <huangbaron2@gmail.com>
2020-10-18 06:38:10 -0400

    home timer is correct

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Header from './Header'
 import './App.css';
-import Carousel from 'react-bootstrap/Carousel'
+import {Carousel} from 'grommet'
 
 const textArray = ["Clothes", "Shoes", "Watches", "Bags"];
 
@@ -27,36 +27,32 @@ class Home extends Component{
                 <br/>
                 <h1 className = "discover">Discover new <h1 className = "wearables">{textThatChanges}</h1></h1>
                 <br/>
-                    <Carousel interval="2500" touch="false" pause="false">
-                    <Carousel.Item>
-                            <img style = {{ paddingTop: "4vh", maxHeight: "470px", maxWidth: "1000px", objectFit: "contain"}}
-                                className="d-block w-50 m-auto"
-                                src="https://cache.mrporter.com/variants/images/10516758728013860/in/w2000_q80.jpg"
-                                alt="Third slide"
+                    <Carousel play="2500" controls = "arrows" initialChild = {0}>
+                       
+                            <img style = {{paddingTop: "25px", maxHeight: "500px", maxWidth: "1000px", objectFit: "contain"}}
+                                className="d-block w-50 m-auto h-50"
+                                src="https://www.thefashionisto.com/wp-content/uploads/2019/04/Acne-Studios-Navid-Logo-Print-Stretch-Jersey-T-Shirt-Men-White.jpg"
+                                alt="First slide"
                             />
-                        </Carousel.Item>
-                        <Carousel.Item>
+     
                             <img style = {{maxHeight: "400px", maxWidth: "1000px", objectFit: "contain"}}
                                 className="d-block w-50 m-auto"
                                 src="https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/cache=expiry:max/rotate=deg:exif/resize=width:2400,fit:crop/output=quality:70/compress/https://process.fs.grailed.com/z0qM3P5pR3a9viT9MCon"
                                 alt="Third slide"
                             />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img style = {{maxHeight: "480px", maxWidth: "1000px", objectFit: "contain"}}
+    
+                            <img style = {{maxHeight: "500px", maxWidth: "1000px", objectFit: "contain"}}
                                 className="d-block w-50 m-auto"
                                 src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/FU6A2?wid=1144&hei=1144&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1555353617178"
                                 alt="Third slide"
                             />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <br/>
-                            <img style = {{paddingTop: "25px", maxHeight: "400px", maxWidth: "1000px", objectFit: "contain"}}
-                                className="d-block w-50 m-auto h-50"
+
+                            <img style = {{paddingTop: "45px", maxHeight: "430px", maxWidth: "1000px", objectFit: "contain"}}
+                                className="d-block w-50 m-auto"
                                 src="https://cdn.bike24.net/i/mb/90/49/44/335783-00-d-708524.jpg"
-                                alt="First slide"
+                                alt="Third slide"
                             />
-                        </Carousel.Item>
+    
                     </Carousel>
             </div>
         </div>
